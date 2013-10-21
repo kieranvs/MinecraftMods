a05ef42f4d97713b7d204a115a7e9d7ba2269b36
kieranvs <ksockalingam@hotmail.com>
2013-10-21 18:40:48 +0100

    Fountains!

--- CastleDecorations/src/castledecorations/block/TileEntityFountain.java
+++ CastleDecorations/src/castledecorations/block/TileEntityFountain.java
@@ -1,7 +1,36 @@
 package castledecorations.block;
 
+import java.util.Random;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.EntitySplashFX;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityFountain extends TileEntity {
+	
+	private Random rand = new Random();
+	
+	@Override
+	public void updateEntity(){
+		if(worldObj.isBlockIndirectlyGettingPowered(this.xCoord, this.yCoord, this.zCoord)){
+			if(FMLCommonHandler.instance().getEffectiveSide().isClient()){
+				if(worldObj.getBlockId(this.xCoord, this.yCoord + 1, this.zCoord) == 9){
+					int num = rand.nextInt(200);
+					for(int i = 0; i < num; i++){
+						float particlex = (float)this.xCoord + rand.nextFloat();
+						float particley = (float)this.yCoord + 1 + rand.nextFloat() * worldObj.getBlockPowerInput(this.xCoord, this.yCoord, this.zCoord);
+						float particlez = (float)this.zCoord + rand.nextFloat();
+						Minecraft.getMinecraft().effectRenderer.addEffect(new EntitySplashFX(Minecraft.getMinecraft().theWorld, particlex, particley, particlez, 0, 1, 0));
+					}
+				}
+			}
+		}
+	}
+	
+	@Override
+	public boolean canUpdate(){
+		return true;
+	}
 
 }
