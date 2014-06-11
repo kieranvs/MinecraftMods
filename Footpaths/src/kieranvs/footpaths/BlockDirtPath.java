1b8312b284cfe6f6819792a2732907f63cf57df4
Kieran <ksockalingam@hotmail.com>
2014-06-11 21:26:16 +0100

    BlockDirtPath drops dirt

--- Footpaths/src/kieranvs/footpaths/BlockDirtPath.java
+++ Footpaths/src/kieranvs/footpaths/BlockDirtPath.java
@@ -1,8 +1,16 @@
 package kieranvs.footpaths;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
 
 public class BlockDirtPath extends Block {
 
@@ -12,4 +20,9 @@ public class BlockDirtPath extends Block {
 		this.setStepSound(soundTypeGrass);
 	}
 
+	@Override
+	public Item getItemDropped(int a, Random b, int c) {
+		return Item.getItemFromBlock(Blocks.dirt);
+	}
+	
 }
